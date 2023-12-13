acf22ae91468982dd7092f072339b56c8e68a2f6
Jens Maker Adventures <75788455+JensMakerAdventures@users.noreply.github.com>
2023-12-13 14:25:06 +0100

    Update SatisfyingPinball.ino
    Final version for video

--- SatisfyingPinball.ino
+++ SatisfyingPinball.ino
@@ -1,37 +1,51 @@
-/*
-  Blink
+// Jens Maker Adventures
+// Code for my satisfying steel marble launching toy
+// 13-12-2023
 
-  Turns an LED on for one second, then off for one second, repeatedly.
+#define stepPin 2
+#define dirPin 5 
+#define rotaryPinA 7
+#define rotaryPinB 6
 
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino
-  model, check the Technical Specs of your board at:
-  https://www.arduino.cc/en/Main/Products
+// TMC2208 or similar stepper controller is necessary to control the stepper motor
 
-  modified 8 May 2014
-  by Scott Fitzgerald
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  modified 8 Sep 2016
-  by Colby Newman
+int speedMax = 300; // us delay between steps. This is the lowest we can go for max speed
+int speedMin = 3500; // us
+int speed = 2500; // us
 
-  This example code is in the public domain.
-
-  https://www.arduino.cc/en/Tutorial/BuiltInExamples/Blink
-*/
-
-// the setup function runs once when you press reset or power the board
+bool aLastState = false;
+ 
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(2, OUTPUT);
+  Serial.begin(9600);
+  pinMode(stepPin,OUTPUT); 
+  pinMode(dirPin,OUTPUT);
+  pinMode(rotaryPinA,INPUT);
+  pinMode(rotaryPinB,INPUT);
+  digitalWrite(dirPin,HIGH); // Set the motor to move in the right direction
 }
 
-// the loop function runs over and over again forever
 void loop() {
-  digitalWrite(2, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(500);                      // wait for a second
-  digitalWrite(2, LOW);   // turn the LED off by making the voltage LOW
-  delay(4000);                      // wait for a second
+  if(speed>(speedMin-200)) // disable motion if speed is set low enough
+  {
+    digitalWrite(stepPin,HIGH); 
+    delayMicroseconds(speed);    // by changing this time delay between the steps we can change the rotation speed
+    digitalWrite(stepPin,LOW); 
+    delayMicroseconds(speed); 
+  }
+
+  if(digitalRead(rotaryPinA) != aLastState) // movement detected
+  {
+    if(digitalRead(rotaryPinB) != aLastState) //clockwise
+    {
+      
+      if(speed < speedMin) {speed = speed + 100;}
+      else { speed = speedMin;}
+    }
+    else // counterclockwise
+    {
+      if(speed > speedMax) {speed = speed - 100;} 
+      else { speed = speedMax;}
+    }
+    aLastState = digitalRead(rotaryPinA);
+  }  
 }
